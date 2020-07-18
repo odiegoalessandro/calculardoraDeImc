baccdff061778f575ca47116c0bf477c111cde78
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2020-07-18 02:25:17 -0300

    Modificando modal

--- Controller/index.js
+++ Controller/index.js
@@ -2,18 +2,24 @@ var btn_calc = document.getElementById('btn--calc')
 var table = document.getElementById('table')
 var btn_clear = document.getElementById('btn--clear')
 var corpo = document.querySelector('body')
-
+var box = document.getElementById('box')
+var modal = document.getElementById('modal')
+var btn_exit = document.querySelector('#btn--exit')
 btn_calc.addEventListener('click', () => {
+    let height = document.getElementById('input--height').value
+    let weight = document.getElementById('input--weight').value
     if(table.hasChildNodes()){
         table.removeChild(table.lastChild)
     }
-    let height = document.getElementById('input--height').value
-    let weight = document.getElementById('input--weight').value
     let imc_text = document.createElement('h3')
-   
     imc_text.classList.add('imc-category')
     let imc = (weight / (height * height)).toFixed(1)
-    
+    var imc_value = document.getElementById('imc-value')
+    imc_value.innerText = imc
+    if(height.length < 1|| weight.length < 1){
+        alert('Numero Invalido')
+        imc_value.innerText = ""
+    }    
     if(imc < 18.5){
         imc_text.innerText = "Magreza"
     }
@@ -29,26 +35,21 @@ btn_calc.addEventListener('click', () => {
     else if(imc > 40){
         imc_text.innerText = "Obesidade III"
     }
-    let imc_value = document.getElementById('imc-value')
-    imc_value.innerText = imc
     table.appendChild(imc_text)
 })
 btn_clear.addEventListener('click', () => {
-    let height = document.getElementById('input--height').value = ""
-    let weight = document.getElementById('input--weight').value = ""
-    let imc_text = document.createElement('h3')
-    let imc_value = document.getElementById('imc-value')
+    document.getElementById('input--height').value = ""
+    document.getElementById('input--weight').value = ""
+    document.getElementById('imc-value').innerText = ""
     if(table.hasChildNodes){
         table.removeChild(table.lastChild)
     }
-    imc_value.innerText = ""
 })
-
-var box = document.getElementById('box')
-var modal = document.getElementById('modal')
-var btn_exit = document.querySelector('#btn--exit')
-
 btn_exit.addEventListener('click', () => {
+    box.classList.add('fecha')
+    modal.classList.add('fecha')
+})
+corpo.addEventListener('click', () => {
     box.classList.add('fecha')
     modal.classList.add('fecha')
 })
